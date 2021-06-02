8c49c1a75ea1d5cd0c062b570cb9084359130d2b
ahmedx3 <ahmed.moustafaa99@gmail.com>
2021-06-02 15:13:32 +0200

    Finished welcome and not found pages

--- instabug/src/views/404.vue
+++ instabug/src/views/404.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <v-btn depressed color="red">
-      404
-    </v-btn>
-  </div>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col justify="center" align="center">
+        <v-img src="../assets/NotFound.jpg" max-height="400" contain></v-img>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center" class="ma-4">
+      <h1 class="h2 indigo--text text--darken-4">404 - Page Not Found!</h1>
+    </v-row>
+    <v-row justify="center" align="center">
+      <h1 class="body-1 indigo--text text--darken-1">
+        Sorry, that page doesn't exist. What would you like to do?
+      </h1>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
--- instabug/src/views/Welcome.vue
+++ instabug/src/views/Welcome.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
-    welcome
-    <v-btn depressed color="primary" @click="logOutAction">
-      Log out
-    </v-btn>
-  </div>
+  <v-container>
+    <v-row justify="center" align="center">
+      <h1 class="h2 indigo--text text--darken-4">Welcome</h1>
+    </v-row>
+    <v-row justify="center" align="center">
+      <h1 class="headline indigo--text text--darken-4">{{ userEmail }}</h1>
+    </v-row>
+    <v-row justify="center" align="center" class="ma-4"> </v-row>
+    <v-row justify="center" align="center">
+      <v-btn depressed color="primary" @click="logOutAction">
+        Log out
+      </v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      userEmail: '',
+    };
   },
   methods: {
     logOutAction() {
@@ -22,6 +32,10 @@ export default {
     if (!localStorage.getItem('user')) {
       this.$router.push('/login');
     }
+  },
+  created() {
+    console.log();
+    this.userEmail = JSON.parse(localStorage.getItem('user')).email;
   },
 };
 </script>
